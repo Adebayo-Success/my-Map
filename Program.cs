d5edeb77346287b780489801d1b8f2c2a482e63a
Adebayo khalid <khalidadebayo18@gmail.com>
2024-12-28 04:35:47 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+mapAlphabetToWord();
+void mapAlphabetToWord()
+{
+    
+    Dictionary<char, string> mapAlphabet = new Dictionary<char, string>()
+    {
+        {'A',"Ant"},
+       {'B' , "Bat"},
+       {'C', "Cat"},
+       {'D', "Dog"},
+        {'E', "Elephant"},
+        {'F', "Fox"},
+        {'G', "Goat"},
+        {'H', "Hyena"},
+        {'I', "Iglow"},
+        {'J', "Jaguar"},
+        {'K', "Kangaroo"},
+      { 'L',"Leopard"},
+      {'M', "Monkey"},
+      {'N', "NeedleFish"},
+      {'O', "Owl"},
+      {'P', "Pig"},
+      {'Q', "Quail"},
+      {'R', "Rat"},
+      {'S', "Shark"},
+      {'T', "Turtle"},
+      {'U', "Umbrellabird"},
+      {'V', "Vulture"},
+      {'W', "Whale"},
+      {'X', "Xema"},
+      {'Y', "Yak"},
+      {'Z', "Zebra"}
+    };
+    int count = 0;
+    foreach (var c in mapAlphabet)
+    {
+        count++;
+        System.Console.Write($" {count}.  {c.Key} ");
+        System.Console.WriteLine($"for {c.Value}");
+    }
+}
+
